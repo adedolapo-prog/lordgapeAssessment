600eefb6e1d320c3fd41f2c23c3459d3bd7d9a9d
Adedolapo <adedolapo@Adedolapos-MacBook-Pro.local>
2021-05-18 07:32:56 +0100

    includes some comments

--- getapi.js
+++ getapi.js
@@ -3,7 +3,7 @@ const http = require('http')
 let Request = require("request")
 const Stream = require('stream')
 
-
+//requests for the api
 Request.get("https://coderbyte.com/api/challenges/json/age-counting", (error, response, body) => {
     if(error) {
         return console.dir(error);
@@ -14,14 +14,16 @@ Request.get("https://coderbyte.com/api/challenges/json/age-counting", (error, re
     
     // console.log(singleData);
 
+    //this is to count the number of elements with age = 32
     let count = 0
     for( let index = 0; index < singleData.length; index++) {
         if (singleData[index] == 'age=32,') {
             count++ 
         }
     }
-    console.log(count)
+    console.log(count) //gives a count of 6
     
+    //creating a readstream and writestream
     let myWriteStream = fs.createWriteStream(__dirname + '/output.txt')
     const readable = new Stream.Readable()
 
